822ff9ad2eed2cbd209d28228be4673ca853f139
Grzegorz Kazulak <grzegorz.kazulak@gmail.com>
2009-05-13 14:20:28 +0200

    pagination_fu module

--- lib/pagination_fu.rb
+++ lib/pagination_fu.rb
@@ -1,1 +1,14 @@
 # PaginationFu
+require 'active_support'
+
+module PaginationFu
+  class << self
+    def run
+      # code
+    end
+  end
+end
+
+if defined?(Rails) and defined?(ActiveRecord) and defined?(ActionController)
+  PaginationFu.run
+end
